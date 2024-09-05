4d3300636d75af5f2119dbf1772451f0c5c8db12
Joseph Nguyen <joseph.h.nguyen@sjsu.edu>
2024-09-05 15:44:41 -0700

    Components Started

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Contact from "./Contact"
+
+const ContactList = ({data, currentPage, getAllContacts}) => {
+  return (
+    <main className='main'>
+      {data?.content?.length === 0 && <div>No Contacts. Add a New Contact.</div>}
+
+      <ul className='contact__list'>
+         {data?.content?.length > 0 && data.content.map(contact => <Contact contact={contact} key={contact.id}/>)}
+      </ul>
+
+      {data?.content?.length > 0 && data?.totalPages > 1 &&
+      <div className='pagination'>
+         <a onClick={() =>getAllContacts(currentPage - 1)} className={0 === currentPage ? 'disabled' : ''}> &laquo;</a>
+         <a onClick={() =>getAllContacts(currentPage + 1)} className={data.totalPages === currentPage + 1 ? 'disabled' : ''}> &raquo;</a>
+      </div>
+      }
+    </main>
+  )
+}
+
+export default ContactList
